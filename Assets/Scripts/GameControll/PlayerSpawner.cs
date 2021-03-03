9a06312db2d527e8d2e0d4d37508a23b8a71a2d5
ebdivergent <ebdivergent@gmail.com>
2021-03-03 13:57:02 +0200

    Update
    Add Animation

---
+++ Assets/Scripts/GameControll/PlayerSpawner.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using Photon.Pun;
+using UnityEngine;
+
+namespace GameEpicFail
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject _playerPref;
+        [SerializeField] CinemachineFreeLook _playerCamera;
+        void Start()
+        {
+            var player = PhotonNetwork.Instantiate(_playerPref.name, Vector3.zero, Quaternion.identity);
+            _playerCamera.Follow = player.transform;
+            _playerCamera.LookAt = player.transform;
+        }
+    }
+}
+
